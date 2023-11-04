4c2da17856ecbb70a7562feb55a2e065e0107308
Mei-2513 <j-uliana9@hotmail.com>
2023-11-03 20:22:14 -0500

    correccion de clases en los paquetes mvc

--- src/model/Food.java
+++ src/model/Food.java
@@ -1,37 +1,5 @@
 package model;
 
-import java.awt.Point;
-import java.util.Random;
-
 public class Food {
-    private Point position;
-    private int foodValue;
 
-    public Food() {
-   
-        respawn();
-    }
-
-    public Point getPosition() {
-        return position;
-    }
-
-    public int getFoodValue() {
-        return foodValue;
-    }
-
-    public void respawn() {
-        // Implementa la lógica para hacer que la comida aparezca en una nueva posición
-        Random random = new Random();
-        int x = random.nextInt(GameConfig.GAME_WIDTH);
-        int y = random.nextInt(GameConfig.GAME_HEIGHT);
-        position = new Point(x, y);
-        foodValue = generateFoodValue(); // Asigna un valor aleatorio a la comida
-    }
-
-    private int generateFoodValue() {
-        // Implementa la lógica para asignar un valor aleatorio a la comida
-        Random random = new Random();
-        return random.nextInt(GameConfig.MAX_FOOD_VALUE) + 1;
-    }
 }
